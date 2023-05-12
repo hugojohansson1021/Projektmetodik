129d19890f1c4242a577008bd7fa8a09b8dce7ff
Grallets <granitlleshi95@hotmail.com>
2023-05-12 19:36:24 +0200

    Added: edit saved notes

--- scripts/load.js
+++ scripts/load.js
@@ -16,13 +16,55 @@ function loadNotes() {
   // Create an empty string to hold html
   let appendString = "";
 
+
   for(let i = notes.length-1; i >= 0; i--) {
-    // Create a div with title and text for each note
     const note = notes[i];
-    appendString += `<div class='note'><h3>${note.title}</h3><p>${note.text}</p><br><a href="#" onclick="deleteNote(${i})">delete</a></div>`;
+    appendString += `<div class='note'>
+      <h3 contenteditable="true">${note.title}</h3>
+      <p contenteditable="true">${note.text}</p><br>
+      <button onclick="saveNote(${i})">Save</button>
+      <a href="#" onclick="deleteNote(${i})">delete</a>
+    </div>`;
   }
+  
 
   // Set the innerhtml of notediv to the appendstring
   noteDiv.innerHTML = appendString;
 
 }
+
+function editNote(index, field, value) {
+  // Get notes from localStorage
+  let notes = localStorage.getItem("saved-notes");
+  notes = notes == undefined ? [] : JSON.parse(notes);
+
+  // Update the note
+  notes[index][field] = value;
+
+  // Save notes back to localStorage
+  localStorage.setItem("saved-notes", JSON.stringify(notes));
+}
+
+
+function saveNote(index) {
+  // Get notes from localStorage
+  let notes = localStorage.getItem("saved-notes");
+  notes = notes == undefined ? [] : JSON.parse(notes);
+
+  // Get the updated title and text directly from the note's content in the HTML
+  const updatedTitle = document.querySelectorAll('.note h3')[index].innerText;
+  const updatedText = document.querySelectorAll('.note p')[index].innerText;
+
+  // Update the note
+  notes[index].title = updatedTitle;
+  notes[index].text = updatedText;
+
+  // Save notes back to localStorage
+  localStorage.setItem("saved-notes", JSON.stringify(notes));
+
+  // Optionally, you can reload the notes to show the updated note
+  loadNotes();
+}
+
+
+
